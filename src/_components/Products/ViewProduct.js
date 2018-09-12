d18cf3b538e569dda5dc338848acf99ac3a7643f
Andy Sekulovski <andy.sek94@gmail.com>
2018-09-12 16:14:35 +1000

    updated product info placeholder text

--- src/_components/Products/ViewProduct.js
+++ src/_components/Products/ViewProduct.js
@@ -15,17 +15,17 @@ class ViewProduct extends Component {
 
                 <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
                 <div>
-                    <h1 className="text-left">Toaster <span className="p-view-header-brand">by Phillips</span></h1>
+                    <h1 className="text-left">iPhone X <span className="p-view-header-brand">by Apple</span></h1>
                     <h5 className="text-left"><span className="fa fa-star pl-1 pr-1 p-view-star"></span> 4.7</h5>
                     <hr />
                 </div>
                <div className="row marg-top-25">
-                    <div className="col-lg-4 col-md-4">
+                    <div className="col-lg-4 col-md-12">
                         <div className="w-100 h-100 cog-radius">
                            <img width="100%" height="400" />
                         </div>
                     </div>
-                    <div className="col-lg-3 col-md-3">
+                    <div className="col-lg-5 col-md-6">
                         <h5 className="text-left">Product Description</h5>
                         <p className="text-left mt-4 p-view-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 

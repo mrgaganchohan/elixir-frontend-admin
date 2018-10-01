de5bd4cb9ab8e850254f9283450379df4369c98d
Andy Sekulovski <andy.sek94@gmail.com>
2018-10-01 14:55:14 +1000

    added rating starts to application

--- src/_components/Products/ProductTableRow.js
+++ src/_components/Products/ProductTableRow.js
@@ -16,7 +16,18 @@ class ProductTableRow extends Component {
                 <td className="text-left col-lg-4 pt-2 pb-1">{this.props.productInfo.name}</td>
                 <td className="col-lg-2 text-left pt-2 pb-1">{this.props.productInfo.brand}</td>
                 <td className="col-lg-2 text-left pt-2 pb-1">{this.props.productInfo.categoryName}</td>
-                <td className="col-lg-1 pt-2 pb-1">{this.props.productInfo.rating}</td>
+                <td className="col-lg-1 pt-2 pb-1">
+                <StarRatingComponent 
+                    name="rate1" 
+                    starCount={5}
+                    value={this.props.productInfo.rating}
+                    editing={false}
+                    starColor={"#f1c40f"}
+                    renderStarIcon={() => <span className="fa fa-star"></span>}
+                    emptyStarColor={"#7f8c8d"}
+                    />
+                
+                </td>
                 <td className="col-lg-3 text-center pt-2 pb-1">
                     <Link className="btn btn-outline-info cog-radius pl-4 pr-4 mr-2 pt-1 pb-1" to={`/product/view/${this.props.productInfo.id}`}>
                     <span className="fa fa-eye pr-2"></span> View
--- src/_components/Products/ViewProductInfo.js
+++ src/_components/Products/ViewProductInfo.js
@@ -4,6 +4,8 @@ import SettingsButton from '../Settings/SettingsButton';
 import "../../assets/css/custom.css";
 import {Link} from 'react-router-dom';
 import ProductCarousel from './ProductCarousel';
+import StarRatingComponent from 'react-star-rating-component';
+
 
 const ViewProductInfo = (data) => { 
 
@@ -16,7 +18,14 @@ const ViewProductInfo = (data) => {
       <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
         <div>
             <h1 className="text-left">{data.data.product.name}<span className="p-view-header-brand"> by {data.data.product.brand}</span></h1>
-            <h5 className="text-left"><span className="fa fa-star pl-1 pr-1 p-view-star"></span>{data.data.product.rating}</h5>
+            <h4 className="text-left"><StarRatingComponent 
+                    name="rate1" 
+                    starCount={5}
+                    value={data.data.product.rating}
+                    editing={false}
+                    starColor={"#f1c40f"}
+                    renderStarIcon={() => <span className="fa fa-star"></span>}
+                    emptyStarColor={"#7f8c8d"}/></h4>
             <hr />
         </div>
         <div className="row marg-top-25">

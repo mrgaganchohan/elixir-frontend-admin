e5add3eb732db5ce58045d0c1849f45ec0b7d3b9
Mirko <mirkopalancaji@gmail.com>
2018-10-04 09:02:27 +1000

    added basic implementation of redux forms

---
+++ src/_components/Products/AddProductForm.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createProduct } from '../../actions/productActions';
+
+class AddProductForm extends Component{
+    renderField(field){
+        const { meta: { touched, error } } = field;
+        const productform = `form-group className="form-group col-lg-12 col-md-12 form-group w-75" ${touched && error ? 'has-danger' : ''}`
+        // console.log(field)
+        return(
+            <div className={productform}>
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type={field.type}
+                    {...field.input}
+                    
+                    />
+                    <div className="text-help">
+                        {touched ? error : ''}
+                    </div>
+
+            </div>
+        );
+    }
+
+    onSubmit(values){
+        // this.props.createProduct(values);
+        console.log(values);
+    }
+
+    render(){
+        const { handleSubmit } = this.props;
+        return(
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <div className="col-lg-5 col-md-6">
+                    <Field
+                        label="Product Name"
+                        name="productName"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Product ID"
+                        name="productId"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Brand Name"
+                        name="brandName"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Category"
+                        name="category"
+                        type="radio"
+                        component={this.renderField}
+                    />
+
+                    <Field
+                        label="Subcategory"
+                        name="subcategory"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Rating"
+                        name="rating"
+                        type="text"
+                        component={this.renderField}
+                    />
+                        <div>
+                            <label>Status</label>
+                            <div>
+                            <label>
+                                <Field
+                                name="status"
+                                component="input"
+                                type="radio"
+                                value="Active"
+                                />{' '}
+                                Active
+                            </label>
+                            <label>
+                                <Field
+                                name="status"
+                                component="input"
+                                type="radio"
+                                value="Inactive"
+                                />{' '}
+                                Inactive
+                            </label>
+                            </div>
+                        </div>
+                    <Field
+                        label="Product Description"
+                        name="productDescription"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Price"
+                        name="price"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Discount"
+                        name="discount"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Upload Images"
+                        name="uploadImg"
+                        type="file"
+                        component={this.renderField}
+                    />
+                        
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        
+            )};
+}
+
+function validate(values){
+    const errors = {};
+}
+
+export default reduxForm({
+    validate, 
+    form: 'AddProductForm'
+}) (connect(null, { createProduct })(AddProductForm));
+// get the initial state
+// map subcategory state
+// radio btn
+// validation
+// 
